bb8be0854ef04801e8e84db0f886b728a2ecf753
Sasha Zbrozek <s.zbrozek@gmail.com>
2025-11-26 18:44:56 -0500

    Formatting and interrupt-safety
    GPIO peripheral configuration was using read-modify-write for registers affecting the whole port. If another GPIO configuration request came through while this one was interrupted, there could be non-deterministic results.

--- drivers/pin.c
+++ drivers/pin.c
@@ -1,15 +1,20 @@
+// This driver provides GPIO pin configuration and control. It does not attempt
+// to validate user inputs; passing a null pointer to any function will likely
+// cause a crash.
+
 #include "pin.h"
+
 #include "stm32.h"
 
 // Atomic set pin high.
-extern inline void Pin_SetHigh(Pin *pin);
+extern inline void Pin_SetHigh(Pin* pin);
 
 // Atomic set pin low.
-extern inline void Pin_SetLow(Pin *pin);
+extern inline void Pin_SetLow(Pin* pin);
 
 // Non-atomic toggle pin.
-void Pin_Toggle(Pin *pin) {
-  if(pin->port->ODR & (0x1U << pin->pin_num)) {
+void Pin_Toggle(Pin* pin) {
+  if (pin->port->ODR & (0x1U << pin->pin_num)) {
     Pin_SetLow(pin);
   } else {
     Pin_SetHigh(pin);
@@ -17,8 +22,8 @@ void Pin_Toggle(Pin *pin) {
 }
 
 // Non-atomic set pin to value.
-void Pin_Set(Pin *pin, bool value) {
-  if(value) {
+void Pin_Set(Pin* pin, bool value) {
+  if (value) {
     Pin_SetHigh(pin);
   } else {
     Pin_SetLow(pin);
@@ -26,34 +31,43 @@ void Pin_Set(Pin *pin, bool value) {
 }
 
 // Read the digital value of a pin.
-bool Pin_Read(Pin *pin) {
+bool Pin_Read(Pin* pin) {
   return (pin->port->IDR & (1 << pin->pin_num)) ? true : false;
 }
 
 // Configure a GPIO pin with the given parameters.
-void Pin_ConfigGpioPin(Pin *pin, PinMode mode, PinOutputType output_type,
-    PinSpeed speed, PinPull pull, uint32_t af) {
+void Pin_ConfigGpioPin(Pin* pin, PinMode mode, PinOutputType output_type,
+                       PinSpeed speed, PinPull pull, uint32_t af) {
+  
+  // Disable interrupts. Configuration below is read-modify-write and is not
+  // atomic. These operations are not interrupt-safe.
+  uint32_t primask = __get_PRIMASK();
+  __disable_irq();
+
   // Enable the clock for this GPIO.
   uint32_t port_num = ((uint32_t)(pin->port) - AHB1PERIPH_BASE) >> 10;
   RCC->AHB1ENR |= (1U << port_num);
 
   // Assign the simple GPIO parameters
   pin->port->MODER = (pin->port->MODER & ~(3U << (2 * pin->pin_num))) |
-      (mode << (2 * pin->pin_num));
+                     (mode << (2 * pin->pin_num));
   pin->port->OTYPER = (pin->port->OTYPER & ~(1U << pin->pin_num)) |
-      (output_type << pin->pin_num);
+                      (output_type << pin->pin_num);
   pin->port->OSPEEDR = (pin->port->OSPEEDR & ~(3U << (2 * pin->pin_num))) |
-      (speed << (2 * pin->pin_num));
+                       (speed << (2 * pin->pin_num));
   pin->port->PUPDR = (pin->port->PUPDR & ~(3U << (2 * pin->pin_num))) |
-      (pull << (2 * pin->pin_num));
+                     (pull << (2 * pin->pin_num));
 
   // The alternate function configuration spans two registers, so get fancy.
-  if(pin->pin_num <= 7) {
+  if (pin->pin_num <= 7) {
     pin->port->AFR[0] = (pin->port->AFR[0] & ~(0xF << (4 * pin->pin_num))) |
-      (af << (4 * pin->pin_num));
+                        (af << (4 * pin->pin_num));
   } else {
     pin->port->AFR[1] =
-      (pin->port->AFR[1] & ~(0xF << (4 * (pin->pin_num - 8)))) |
-      (af << (4 * (pin->pin_num - 8)));
+        (pin->port->AFR[1] & ~(0xF << (4 * (pin->pin_num - 8)))) |
+        (af << (4 * (pin->pin_num - 8)));
   }
+
+  // Re-enable interrupts.
+  __set_PRIMASK(primask);
 }
