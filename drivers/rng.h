7476f945da69b165173d0e5237d2a08769a8854d
Sasha Zbrozek <s.zbrozek@gmail.com>
2025-11-27 10:25:43 -0500

    Ran clangd-format

--- drivers/rng.h
+++ drivers/rng.h
@@ -6,6 +6,6 @@
 
 // Uses the hardware random number generator (RNG) to get 32 bits of entropy.
 // Returned bool indicates whether the process succeeded or failed.
-bool RNG_GetRand32(uint32_t *return_value);
+bool RNG_GetRand32(uint32_t* return_value);
 
 #endif
